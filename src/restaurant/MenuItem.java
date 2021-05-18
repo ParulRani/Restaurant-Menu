b4c6eb125251cf9cd5764ee2528953d9d3b479b3
Parul Rani <rathiparul90@gmail.com>
2021-05-17 21:52:31 -0500

    studio excercise

---
+++ src/restaurant/MenuItem.java
@@ -0,0 +1,17 @@
+package restaurant;
+
+import java.util.ArrayList;
+
+public class MenuItem {
+private
+    double price;
+    String description;
+    String category;
+    MenuItem( double price, String description, String category)
+    {
+        this.price=price;
+        this.description=description;
+        this.category=category;
+
+    }
+}
